dc38e91f680f1d24b647496bdbaa9b6a3ad5f1ec
DELL <adarsh.kulal01@gmail.com>
2025-02-08 00:31:52 +0530

    pageFactory

---
+++ test/tests/vwoLogin/conftest.py
@@ -0,0 +1,24 @@
+# it's for Page Factory
+
+from selenium import webdriver
+from selenium.webdriver import Chrome
+import pytest
+
+import os
+from dotenv import  load_dotenv
+load_dotenv()
+
+driver = webdriver.Edge()
+
+@pytest.fixture(scope='class')
+def setup(request):
+    driver.maximize_window()
+    username = os.getenv("USERNAME12")
+    password = os.getenv("PASSWORD12")
+
+    request.cls.driver = driver
+    request.cls.username = username
+    request.cls.password = password
+
+    yield driver  # return driver
+    driver.quit()
